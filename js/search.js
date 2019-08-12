5af0dc1131edc625e37390c28c0e24a23dfe83ad
Rakthy Thip <campinamerica@yahoo.com>
2019-08-12 15:36:42 -0700

    working on local storage
    local storage with select class doesn't get value..

--- js/search.js
+++ js/search.js
@@ -1,4 +1,8 @@
 const search = document.getElementById("search");
+const form = document.getElementById('widget-container');
+const userSearch = document.getElementById('userField');
+const messageUser = document.getElementById('messageField');
+
 
 search.addEventListener("keyup", filter);
 function filter() {
@@ -17,27 +21,55 @@ function filter() {
   }
 }
 
-//search for user//
+//alert for user messages//
 
-const form = document.querySelector("#widget-container");
-
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const userSearch = document.querySelector("#userField");
-  userSearch.addEventListener("keyup", userFilter);
-  function userFilter() {
-    const userSearchValue = userSearch.value.toLowerCase();
-    const members = document.querySelectorAll(".members-container");
-    for (let i = 0; i < members.length; i++) {
-      const memberName = document.querySelectorAll(".member-name");
-      for (let i = 0; i < memberName.length; i++) {
-        const memberNameValue = memberName[i].innerHTML.toLowerCase();
-        if (memberNameValue.indexOf(userSearchValue) > -1) {
-          members[i].style.display = "flex";
-        } else {
-          members[i].style.display = "none";
-        }
-      }
-    }
-  }
+form.addEventListener('submit', (e) => {
+e.preventDefault();
+if(userSearch.value ==='' && messageUser.value === ''){
+  swal({
+  title: "Error!",
+  text: "Please fill out user and message field before sending",
+  icon: "error",
 });
+} else if (userSearch.value === ''){
+  swal({
+  title: "Error!",
+  text: "Please fill out user field before sending!",
+  icon: "error",
+});
+} else if (messageUser.value === ''){
+  swal({
+  title: "Error!",
+  text: "Please fill out message field before sending!",
+  icon: "error",
+});
+} else {
+  swal({
+  title: "Success!",
+  text: "Your message has been successfully sent!",
+  icon: "success",
+});
+  userSearch.value = '';
+  messageUser.value = '';
+}
+});
+
+//settings local storage//
+const settings = document.querySelector('.settings');
+const emailCheckBox = document.querySelector('#email-notification');
+const publicCheckBox = document.querySelector('#profile-to-public');
+const selectClass = document.getElementById('select').value;
+const saveBtn = document.querySelector('#save');
+const cancelBtn = document.querySelector('#cancel');
+
+document.addEventListener('DOMContentLoaded', function(){
+saveBtn.addEventListener('click', save);
+function save () {
+  localStorage.setItem('select-time-zone', selectClass);
+  localStorage.setItem('emailCheckBox', emailCheckBox.checked);
+  localStorage.setItem('profile-to-public', publicCheckBox.checked);
+
+}
+})
+
+// var checked = JSON.parse(localStorage.getItem('emailCheckBox'));
