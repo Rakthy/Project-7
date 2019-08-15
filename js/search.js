367c43c64b4f069421c27391390d77eb03d089e4
Rakthy Thip <campinamerica@yahoo.com>
2019-08-14 19:39:11 -0700

    local storage complete
    local storage and alert user message field complete with javascript

--- js/search.js
+++ js/search.js
@@ -1,8 +1,7 @@
 const search = document.getElementById("search");
-const form = document.getElementById('widget-container');
-const userSearch = document.getElementById('userField');
-const messageUser = document.getElementById('messageField');
-
+const form = document.getElementById("widget-container");
+const userSearch = document.getElementById("userField");
+const messageUser = document.getElementById("messageField");
 
 search.addEventListener("keyup", filter);
 function filter() {
@@ -23,53 +22,73 @@ function filter() {
 
 //alert for user messages//
 
-form.addEventListener('submit', (e) => {
-e.preventDefault();
-if(userSearch.value ==='' && messageUser.value === ''){
-  swal({
-  title: "Error!",
-  text: "Please fill out user and message field before sending",
-  icon: "error",
-});
-} else if (userSearch.value === ''){
-  swal({
-  title: "Error!",
-  text: "Please fill out user field before sending!",
-  icon: "error",
-});
-} else if (messageUser.value === ''){
-  swal({
-  title: "Error!",
-  text: "Please fill out message field before sending!",
-  icon: "error",
-});
-} else {
-  swal({
-  title: "Success!",
-  text: "Your message has been successfully sent!",
-  icon: "success",
-});
-  userSearch.value = '';
-  messageUser.value = '';
-}
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  if (userSearch.value === "" && messageUser.value === "") {
+    swal({
+      title: "Error!",
+      text: "Please fill out user and message field before sending",
+      icon: "error"
+    });
+  } else if (userSearch.value === "") {
+    swal({
+      title: "Error!",
+      text: "Please fill out user field before sending!",
+      icon: "error"
+    });
+  } else if (messageUser.value === "") {
+    swal({
+      title: "Error!",
+      text: "Please fill out message field before sending!",
+      icon: "error"
+    });
+  } else {
+    swal({
+      title: "Success!",
+      text: "Your message has been successfully sent!",
+      icon: "success"
+    });
+    userSearch.value = "";
+    messageUser.value = "";
+  }
 });
 
 //settings local storage//
-const settings = document.querySelector('.settings');
-const emailCheckBox = document.querySelector('#email-notification');
-const publicCheckBox = document.querySelector('#profile-to-public');
-const selectClass = document.getElementById('select').value;
-const saveBtn = document.querySelector('#save');
-const cancelBtn = document.querySelector('#cancel');
+const settings = document.querySelector(".settings");
+const emailCheckBox = document.querySelector("#email-notification");
+const profileCheckBox = document.querySelector("#profile-to-public");
+const selectTimeZone = document.getElementById("select");
+const saveBtn = document.querySelector("#save");
+const cancelBtn = document.querySelector("#cancel");
 
-document.addEventListener('DOMContentLoaded', function(){
-saveBtn.addEventListener('click', save);
-function save () {
-  localStorage.setItem('select-time-zone', selectClass);
-  localStorage.setItem('emailCheckBox', emailCheckBox.checked);
-  localStorage.setItem('profile-to-public', publicCheckBox.checked);
-
+saveBtn.addEventListener("click", save);
+function save() {
+  localStorage.setItem("emailCheckBox", emailCheckBox.checked);
+  localStorage.setItem("profile-to-public", profileCheckBox.checked);
+  localStorage.setItem("select-time-zone", selectTimeZone.value);
+  swal({
+    title: "Saved",
+    text: "Your settings has been saved",
+    icon: "success"
+  });
 }
-})
+const saveEmail = localStorage.getItem("emailCheckBox", emailCheckBox.checked);
+const saveProfile = localStorage.getItem("profile-to-public",profileCheckBox.checked);
+const saveTimeZone = localStorage.getItem("select-time-zone",selectTimeZone.value);
 
-// var checked = JSON.parse(localStorage.getItem('emailCheckBox'));
+document.addEventListener("DOMContentLoaded", () => {
+  emailCheckBox.checked = saveEmail === "true";
+  profileCheckBox.checked = saveProfile === "true";
+  selectTimeZone.value = saveTimeZone;
+});
+
+cancelBtn.addEventListener("click", () => {
+  emailCheckBox.checked = "false";
+  profileCheckBox.checked = "false";
+  selectTimeZone.value = "Select Time-Zone";
+  swal({
+    title: "Cancelled!",
+    text: "Your settings has been cancelled",
+    icon: "error"
+  });
+});
